45ee32033144ac072520f3d723a309b0448910ea
Max Yu <18641481+maxyu1115@users.noreply.github.com>
2024-08-09 00:06:58 -0700

    hermitd: reorganize directory structure, and unit tests

---
+++ hermitd/tests/__init__.py
@@ -0,0 +1,5 @@
+import os
+import sys
+
+# hooks up the path to pick up our local python files
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
---
+++ hermitd/tests/test_context.py
@@ -0,0 +1,40 @@
+import pytest
+from hermitd.context import Context
+from hermitd.messages import ShellOutputType
+
+
+@pytest.mark.parametrize(
+    "input, expected_output",
+    [
+        ([(ShellOutputType.Header, "1")], []),
+        ([(ShellOutputType.Input, "1")], [[(ShellOutputType.Input, "1")]]),
+        (
+            [
+                (ShellOutputType.Output, "1"),
+                (ShellOutputType.Input, "2"),
+                (ShellOutputType.Output, "3"),
+            ],
+            [
+                [(ShellOutputType.Output, "1")],
+                [(ShellOutputType.Input, "2"), (ShellOutputType.Output, "3")],
+            ],
+        ),
+        (
+            [
+                (ShellOutputType.Input, "1"),
+                (ShellOutputType.Output, "2"),
+                (ShellOutputType.Input, "3"),
+            ],
+            [
+                [(ShellOutputType.Input, "1"), (ShellOutputType.Output, "2")],
+                [(ShellOutputType.Input, "3")],
+            ],
+        ),
+    ],
+)
+def test_save_shell_context(input, expected_output):
+    ctx = Context()
+    for data_type, data in input:
+        ctx.save_shell_context(data_type, data)
+
+    assert ctx._get_shell_ctx() == expected_output
