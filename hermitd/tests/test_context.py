e71860d2f02cae55a9f143c7bc83eeeb29a160b7
Max Yu <18641481+maxyu1115@users.noreply.github.com>
2024-09-09 17:52:27 -0700

    hermitd: add limit to undecided context size

--- hermitd/tests/test_context.py
+++ hermitd/tests/test_context.py
@@ -1,5 +1,5 @@
 import pytest
-from hermitd.context import Context
+from hermitd.context import Context, TRUNCATED_MARKER
 from hermitd.messages import ShellOutputType
 
 
@@ -46,6 +46,50 @@ from hermitd.messages import ShellOutputType
 )
 def test_save_shell_context(input, expected_output):
     ctx = Context()
+    for data_type, data in input:
+        ctx.save_shell_context(data_type, data)
+
+    assert ctx._get_shell_ctx() == expected_output
+
+
+@pytest.mark.parametrize(
+    "input, expected_output",
+    [
+        (
+            [
+                (None, "0"),
+                (None, "1"),
+                (None, "2"),
+                (None, "3"),
+                (ShellOutputType.Input, "input"),
+            ],
+            [
+                [(ShellOutputType.Input, f"012{TRUNCATED_MARKER}input")],
+            ],
+        ),
+        (
+            [
+                (None, "01234567"),
+                (ShellOutputType.Input, "input"),
+            ],
+            [
+                [(ShellOutputType.Input, f"012{TRUNCATED_MARKER}input")],
+            ],
+        ),
+        (
+            [
+                (None, "012"),
+                (ShellOutputType.Input, "input"),
+            ],
+            [
+                [(ShellOutputType.Input, "012input")],
+            ],
+        ),
+    ],
+)
+def test_save_shell_context_truncation(input, expected_output):
+    ctx = Context(max_chunk_length=3)
+
     for data_type, data in input:
         ctx.save_shell_context(data_type, data)
 
